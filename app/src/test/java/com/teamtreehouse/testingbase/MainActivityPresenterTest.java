9343cea0adc7a080ed5e47dbd170453e355edd4f
roberrera <roberterrera@gmail.com>
2016-11-22 14:35:55 -0500

    Create test editTextUpdated

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
@@ -9,12 +9,40 @@ import static org.junit.Assert.*;
  * Created by Rob on 11/22/16.
  */
 public class MainActivityPresenterTest {
-  @Before public void setUp() throws Exception {
 
+  MainActivityPresenter presenter;
+  MainActivityView view;
+
+  class MockedView implements MainActivityView {
+    String textViewText;
+
+    @Override public void changeTextViewText(String text) {
+        textViewText = text;
+    }
+
+    @Override public void changeBackgroundColor(int color) {
+
+    }
+
+    @Override public void launchOtherActivity(Class activity) {
+
+    }
+  }
+
+  @Before public void setUp() throws Exception {
+    view = new MockedView();
+    presenter = new MainActivityPresenter(view);
   }
 
   @Test public void editTextUpdated() throws Exception {
+    // Arrange
+    String givenString = "test123";
 
+    // Act
+    presenter.editTextUpdated(givenString);
+
+    // Assert
+    assertEquals(givenString, ((MockedView)view).textViewText);
   }
 
   @Test public void colorSelected() throws Exception {
