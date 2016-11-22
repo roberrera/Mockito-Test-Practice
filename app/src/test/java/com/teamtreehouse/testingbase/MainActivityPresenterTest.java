435d7c6ec61fe7d15022cea69975f4ab1437c457
roberrera <roberterrera@gmail.com>
2016-11-22 14:45:15 -0500

    Refactor test to use Mockito

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
@@ -2,35 +2,26 @@ package com.teamtreehouse.testingbase;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Rob on 11/22/16.
  */
+
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityPresenterTest {
 
   MainActivityPresenter presenter;
+
+  @Mock
   MainActivityView view;
 
-  class MockedView implements MainActivityView {
-    String textViewText;
-
-    @Override public void changeTextViewText(String text) {
-        textViewText = text;
-    }
-
-    @Override public void changeBackgroundColor(int color) {
-
-    }
-
-    @Override public void launchOtherActivity(Class activity) {
-
-    }
-  }
-
   @Before public void setUp() throws Exception {
-    view = new MockedView();
     presenter = new MainActivityPresenter(view);
   }
 
@@ -42,7 +33,7 @@ public class MainActivityPresenterTest {
     presenter.editTextUpdated(givenString);
 
     // Assert
-    assertEquals(givenString, ((MockedView)view).textViewText);
+    Mockito.verify(view).changeTextViewText(givenString);
   }
 
   @Test public void colorSelected() throws Exception {
