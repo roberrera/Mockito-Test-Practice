7e4d77448d1ee3e4b56e2abe011e8e67cc504775
roberrera <roberterrera@gmail.com>
2016-11-22 15:47:48 -0500

    Create unit tests with Robolectric

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
@@ -1,15 +1,28 @@
 package com.teamtreehouse.testingbase;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.view.inputmethod.EditorInfo;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Rob on 11/21/16.
  */
 
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivityTest {
 
   MainActivity activity;
@@ -17,8 +30,7 @@ public class MainActivityTest {
   @Before
   public void setup() {
     // Instantiate MainActivity
-    activity = new MainActivity();
-    activity.onCreate(null);
+    activity = Robolectric.setupActivity(MainActivity.class);
   }
 
   @Test
@@ -33,6 +45,38 @@ public class MainActivityTest {
     //Assert
     String actualString = activity.textView.getText().toString();
     assertEquals(givenString, actualString);
+  }
+
+  @Test
+  public void spinnerUpdatesBackgroundColor() throws Exception  {
+    // Arrange
+    int index = 2;
+    int givenColor = Color.GREEN;
+
+    // Act
+    activity.colorSpinner.setSelection(index);
+
+    // Assert
+    // Get the background color of the linear layout's background color
+    int actualColor = ((ColorDrawable)activity.linearLayout.getBackground()).getColor();
+    assertEquals(givenColor, actualColor);
+  }
+
+  @Test
+  public void buttonLaunchesOtherActivity() throws Exception {
+    // Arrange
+    Class clazz = OtherActivity.class;
+    Intent expectedIntent = new Intent(activity, clazz);
+
+    // Act
+    // Test clicking the button
+    activity.launchActivityButton.callOnClick();
+
+    // Assert
+    // Check that the correct intent is called, via a shadow class
+    ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+    Intent actualIntent = shadowActivity.getNextStartedActivity();
+    assertTrue(expectedIntent.filterEquals(actualIntent));
   }
 
 }
